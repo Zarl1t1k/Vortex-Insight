115e848ea095698fb57be4af5ebf6eb7106834e9
Zarl1t1k <zarl1t1k@gmail.com>
2026-04-28 14:14:49 +0300

    Update VortexInsight.java

--- src/main/java/net/vortex/insight/base/VortexInsight.java
+++ src/main/java/net/vortex/insight/base/VortexInsight.java
@@ -2,20 +2,17 @@ package net.vortex.insight.base;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.minecraft.text.Text;
+import net.vortex.insight.gui.ClickGui;
 import org.lwjgl.glfw.GLFW;
 
 public class VortexInsight implements ClientModInitializer {
-    public static boolean isPanicMode = false;
-
     @Override
     public void onInitializeClient() {
-        // Регистрация нажатия клавиши (например, 'P' для проверки)
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (client.player != null) {
-                while (GLFW.glfwGetKey(client.getWindow().getHandle(), GLFW.GLFW_KEY_P) == GLFW.GLFW_PRESS) {
-                    client.player.sendMessage(Text.literal("§b[Vortex] §fСистема работает!"), true);
-                    break;
+            // Если нажат ПРАВЫЙ SHIFT и меню еще не открыто
+            if (client.player != null && GLFW.glfwGetKey(client.getWindow().getHandle(), GLFW.GLFW_KEY_RIGHT_SHIFT) == GLFW.GLFW_PRESS) {
+                if (!(client.currentScreen instanceof ClickGui)) {
+                    client.setScreen(new ClickGui());
                 }
             }
         });
