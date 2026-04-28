a78faefa282e4458fa6ac990b6c78bd627cb79f3
Zarl1t1k <zarl1t1k@gmail.com>
2026-04-28 13:53:09 +0300

    Update PlayerEntityMixin.java

--- src/main/java/net/vortex/insight/mixin/PlayerEntityMixin.java
+++ src/main/java/net/vortex/insight/mixin/PlayerEntityMixin.java
@@ -1,1 +1,16 @@
+package net.vortex.insight.mixin;
 
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerEntity.class)
+public class PlayerEntityMixin {
+    
+    @Inject(method = "getAbilities().getWalkSpeed()", at = @At("HEAD"), cancellable = true)
+    private void onGetSpeed(CallbackInfoReturnable<Float> info) {
+        // Здесь в будущем будет код для SpeedHack
+    }
+}
