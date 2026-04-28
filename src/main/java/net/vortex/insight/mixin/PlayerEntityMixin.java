41373e75d06a8c3848d6152161c59375d865829a
Zarl1t1k <zarl1t1k@gmail.com>
2026-04-28 14:11:51 +0300

    Update PlayerEntityMixin.java

--- src/main/java/net/vortex/insight/mixin/PlayerEntityMixin.java
+++ src/main/java/net/vortex/insight/mixin/PlayerEntityMixin.java
@@ -1,6 +1,7 @@
 package net.vortex.insight.mixin;
 
 import net.minecraft.entity.player.PlayerEntity;
+import net.vortex.insight.modules.ModuleManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -8,16 +9,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin {
+
     @Inject(method = "tick", at = @At("HEAD"))
     private void onTick(CallbackInfo info) {
-        // Превращаем 'this' (текущий объект) в игрока, чтобы управлять им
+        // Превращаем текущий объект в игрока
         PlayerEntity player = (PlayerEntity) (Object) this;
 
-        // Условие для умного спринта:
-        // 1. Игрок нажимает кнопку движения вперед (forwardSpeed > 0)
-        // 2. Игрок не крадется (isSneaking)
-        // 3. Игрок не под водой (чтобы не багал заплыв)
-        if (player.forwardSpeed > 0 && !player.isSneaking() && !player.isSubmergedInWater()) {
+        // Проверяем: 
+        // 1. Включен ли модуль "Sprint" в нашем менеджере
+        // 2. Двигается ли игрок вперед
+        // 3. Не зажата ли клавиша приседания (Shift)
+        if (ModuleManager.isEnabled("Sprint") && player.forwardSpeed > 0 && !player.isSneaking()) {
             player.setSprinting(true);
         }
     }
