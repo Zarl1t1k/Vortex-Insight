85c6434e0a628bd42d02ae65897d3fea0005e4d0
Zarl1t1k <zarl1t1k@gmail.com>
2026-04-28 13:58:20 +0300

    Update LightmapMixin.java

--- src/main/java/net/vortex/insight/mixin/LightmapMixin.java
+++ src/main/java/net/vortex/insight/mixin/LightmapMixin.java
@@ -1,1 +1,16 @@
+package net.vortex.insight.mixin;
 
+import net.minecraft.client.render.GameRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(GameRenderer.class)
+public class LightmapMixin {
+    @Inject(method = "getNightVisionStrength", at = @At("HEAD"), cancellable = true)
+    private static void onGetNightVisionStrength(CallbackInfoReturnable<Float> info) {
+        // Мы заставляем игру думать, что у нас всегда эффект ночного зрения на 100%
+        info.setReturnValue(1.0f);
+    }
+}
